0d0a3206e382605fced5cd83b5b2a42c990dca9a
JK <julitka015@wp.pl>
2017-11-26 17:25:45 +0100

    validation for beting answers

--- PostawNaMilion/Program.cs
+++ PostawNaMilion/Program.cs
@@ -11,65 +11,100 @@ namespace PostawNaMilion
 {
     class Program
     {
+        public Random random;
         public IEnumerable<Category> Categories;
         public int award = 1000000;
-        public int currentQuestionNumber = 1;
+        public int currentQuestionNumber = 0;
 
 
         public void DisplayAwardsLvl()
         {
-            Console.WriteLine("\n           -------POSTAW NA MILION-------");
-            Console.WriteLine("\n          P1   P2  P3  P4  P5  P6  P7  P8");
+            Console.WriteLine("\n           -------POSTAW NA MILION-------\n          ");
+            for(var i = 0; i < currentQuestionNumber; i++)
+            {
+                Console.Write(" x ");
+            }
+            for (var i = 0; i < 8 - currentQuestionNumber; i++)
+            {
+                Console.Write($" {currentQuestionNumber + i + 1} ");
+            }
+
 
         }
 
         public void ChooseCategory()
         {
-            string[] categories = new string[2]; ;
-            for(var i = 0; i < categories.Length; i++)
-            {
-                var types = Categories.Where(cat => cat.NotUsed).Select(cat => cat.Name).ToArray();
-                foreach(var type in types){
-                    categories[i] = type;
-                }
-                
-                Categories.Where(cat => cat.NotUsed).First().NotUsed = false;
-            }
+            string[] categories = new string[2];
 
+            var types = Categories.Where(cat => cat.NotUsed).Select(cat => cat.Name).ToArray();
+            int index1; 
+            int index2;
+            do
+            {
+                index1 = random.Next(types.Length);
+                index2 = random.Next(types.Length);
+            } while (index1 == index2);
+
+            categories[0] = types[index1];
+            categories[1] = types[index2];
+            
             Console.WriteLine($"\n          Wybierz kategorie:\n 1. {categories[0]} \n 2. {categories[1]} \n");
-            string y = Console.ReadLine();
-            int x = int.Parse(y);
-            switch (x)
+            string inputString = Console.ReadLine();
+            int input = int.Parse(inputString);
+            switch (input)
             {
                 case 1:
-                    BetAnswers(categories[0]);
+                    DisplayQuestion(categories[0]);
                     break;
                 case 2:
-                    BetAnswers(categories[1]);
+                    DisplayQuestion(categories[1]);
                     break;
+                default:
+                    Console.WriteLine("Wybierz kategorię ponownie poprzez naciniecie 1 lub 2");
+                    return;
             }
         }
 
-        public void BetAnswers( string category)
+        public void DisplayQuestion(string category)
+        {
+            string randomQuestion;
+            var questions = Categories.Where(cat => cat.Name.Equals(category)).Select(c => c.Questions.Select(cate => cate.Content).ToArray()).ToArray();
+            Categories.Where(cat => cat.Name.Equals(category)).First().NotUsed = false; 
+            
+            var questionsCount = questions.Select(c => c.Length).ToArray()[0];
+            var r = random.Next(questionsCount);
+            randomQuestion = questions[0][r];
+
+            Console.WriteLine($"\n      {category}\n{randomQuestion}\nAnswer 1 ANSWER 2 ANSWER 3\nObstaw odpowiedzi: (Pamiętaj że najmniejsza kwota 10 000)");
+
+            BetAnswers();
+ 
+        }
+
+        public void BetAnswers()
         {
             var sum = 0;
-            var question = Categories.Where(cat => cat.Name.Equals(category)).Select(c => c.Questions.Select(cate => cate.Content).ToArray());
-            foreach(var q in question)
-            {
-                foreach(var qq in q)
-                {
-                    Console.WriteLine(qq);
-                }
-            }
             var arrayBetAmount = new int[3];
             string amount;
             var counter = 0;
-            Console.WriteLine($"\n      {category}\n{question}\nAnswer 1 ANSWER 2 ANSWER 3\nObstaw odpowiedzi: ");
+
+            // var answers = Categories.Where(c => c.Name.Equals(category)).Where(c => c.Questions.Equals(randomQuestion)).Select(cat => cat.Questions.Select(c => c.Answers.Select(cate => cate.Content))).ToArray(); ;
+
             for (int i = 0; i < arrayBetAmount.Length; i++)
             {
+                Console.WriteLine($" Odpowiedz {i + 1}: ");
                 amount = Console.ReadLine();
-                arrayBetAmount[i] = int.Parse(amount);
-                sum += arrayBetAmount[i];
+                var amountInt = int.Parse(amount);
+                if ( amountInt % 10000 == 0)
+                {
+                    arrayBetAmount[i] = amountInt;
+                    sum += arrayBetAmount[i];
+                }
+                else
+                {
+                    Console.WriteLine($"Podaj kwoty, ktore sa wielokrotnościami 10 000 zł");
+                    BetAnswers();
+                }
             }
             for (int i = 0; i < arrayBetAmount.Length; i++)
             {
@@ -81,7 +116,7 @@ namespace PostawNaMilion
             if (counter == 0)
             {
                 Console.WriteLine("Jedna odpowiedz ma byc za 0 zł, spróbuj ponownie");
-                BetAnswers(category);
+                BetAnswers();
             }
             else
             {
@@ -95,13 +130,13 @@ namespace PostawNaMilion
                     else if (Console.ReadLine().Equals("N", StringComparison.InvariantCultureIgnoreCase))
                     {
                         Console.WriteLine("Wprowadź kwoty ponownie");
-                        BetAnswers(category);
+                        BetAnswers();
                     }
                 }
                 else
                 {
                     Console.WriteLine($"Żle rozłozone pieniążki, spróbuj ponownie, masz {award}");
-                    BetAnswers(category);
+                    BetAnswers();
                 }
             }
         }
@@ -124,13 +159,14 @@ namespace PostawNaMilion
         public Program()
         {
             LoadData();
+            random = new Random();
             do
             {
                 DisplayAwardsLvl();
                 ChooseCategory();
                 currentQuestionNumber++;
-                Console.WriteLine(award);
-            } while (award != 0 || currentQuestionNumber < 8);
+            } while (award != 0 && currentQuestionNumber < 8);
+            Console.WriteLine($"WYGRANA {award} zł");
         }
 
         static void Main(string[] args)
